d185cb48e2b4050852d2005a8c74ef6cffc519e1
Guryss <lala1470@konkuk.ac.kr>
2024-10-11 16:26:50 +0900

    [#4] - 곡 추가버튼 눌렀을 때 애플뮤직의 해당 플레이리스트 화면으로 이동하는 딥링크 구현

--- AGAMI/Sources/Presentation/ViewModel/MusicViewModel.swift
+++ AGAMI/Sources/Presentation/ViewModel/MusicViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by Seoyeon Choi on 10/11/24.
 //
 
-import Foundation
+import SwiftUI
 
 import MusicKit
 
@@ -15,7 +15,7 @@ final class MusicViewModel {
     var playlistDescription: String = ""
     var songTitle: String = ""
     var statusMessage: String = ""
-
+    
     private let musicService = MusicService()
     
     func requestAuthrization() {
@@ -32,7 +32,7 @@ final class MusicViewModel {
     func createPlaylist() {
         Task {
             do {
-                let playlist = try await musicService.createPlaylist(name: playlistName, description: playlistDescription)
+                _ = try await musicService.createPlaylist(name: playlistName, description: playlistDescription)
                 statusMessage = "플레이리스트 '\(playlistName)' 생성 완료."
             } catch {
                 statusMessage = "플레이리스트 생성 오류: \(error.localizedDescription)"
@@ -45,9 +45,21 @@ final class MusicViewModel {
             do {
                 try await musicService.searchAndAddSong(songTitle: songTitle)
                 statusMessage = "'\(songTitle)' 곡이 플레이리스트에 추가되었습니다."
+                
+                if let playlistUrl = musicService.getCurrentPlaylistUrl() {
+                    openPlaylist(playlistUrl: playlistUrl)
+                }
             } catch {
                 statusMessage = "곡 추가 오류: \(error.localizedDescription)"
             }
+        }
+    }
+    
+    private func openPlaylist(playlistUrl: String) {
+        guard let url = URL(string: playlistUrl) else { return }
+        
+        DispatchQueue.main.async {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
 }

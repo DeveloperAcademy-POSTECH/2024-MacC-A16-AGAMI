4da45cb0580e5f3d66f40a94e4d02d832c28efc4
00yhsp <00yhsp@naver.com>
2024-10-15 03:26:20 +0900

    [#9] Archive 탭 UI 구현

---
+++ AGAMI/Sources/Coordinator/ArchiveCoordinator.swift
@@ -0,0 +1,87 @@
+//
+//  ArchiveCoordinator.swift
+//  AGAMI
+//
+//  Created by 박현수 on 10/15/24.
+//
+
+import Foundation
+import SwiftUI
+
+enum ArchiveView: Hashable {
+    case listView
+    case playlistView
+}
+
+enum ArchiveSheet: String, Identifiable {
+    var id: String { self.rawValue }
+
+    case dummySheet
+}
+
+enum ArchiveFullScreenCover: String, Identifiable {
+    var id: String { self.rawValue }
+
+    case dummyFullScreenCover
+}
+
+@Observable
+final class ArchiveCoordinator {
+    var path: NavigationPath = .init()
+    var sheet: ArchiveSheet?
+    var fullScreenCover: ArchiveFullScreenCover?
+
+    func push(view: ArchiveView) {
+        path.append(view)
+    }
+
+    func pop() {
+        path.removeLast()
+    }
+
+    func popToRoot() {
+        path.removeLast(path.count)
+    }
+
+    func presentSheet(_ sheet: ArchiveSheet) {
+        self.sheet = sheet
+    }
+
+    func presentFullScreenCover(_ cover: ArchiveFullScreenCover) {
+        self.fullScreenCover = cover
+    }
+
+    func dismissSheet() {
+        self.sheet = nil
+    }
+
+    func dismissFullScreenCover() {
+        self.fullScreenCover = nil
+    }
+
+    @ViewBuilder
+    func build(view: ArchiveView) -> some View {
+        switch view {
+        case .listView:
+            ArchiveListView()
+        case .playlistView:
+            ArchivePlaylistView()
+        }
+    }
+
+    @ViewBuilder
+    func buildSheet(sheet: ArchiveSheet) -> some View {
+        switch sheet {
+        case .dummySheet:
+            EmptyView()
+        }
+    }
+
+    @ViewBuilder
+    func buildFullScreenCover(cover: ArchiveFullScreenCover) -> some View {
+        switch cover {
+        case .dummyFullScreenCover:
+            EmptyView()
+        }
+    }
+}
---
+++ AGAMI/Sources/Presentation/View/Archive/ArchivePlaylistView.swift
@@ -0,0 +1,121 @@
+//
+//  ArchivePlaylistView.swift
+//  AGAMI
+//
+//  Created by 박현수 on 10/15/24.
+//
+
+import SwiftUI
+
+struct ArchivePlaylistView: View {
+    @State var viewModel: ArchivePlaylistViewModel = .init()
+
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 0) {
+                PlaylistImageCellView(viewModel: viewModel)
+                TitleAndDescriptionView(viewModel: viewModel)
+                PlaylistView(viewModel: viewModel)
+            }
+        }
+        .toolbar { EditButton() }
+    }
+}
+
+private struct PlaylistImageCellView: View {
+    let viewModel: ArchivePlaylistViewModel
+
+    var body: some View {
+        AsyncImage(url: viewModel.dummyURL) { image in
+            image
+                .resizable()
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+                .aspectRatio(1, contentMode: .fit)
+                .padding(.horizontal, 20)
+                .shadow(radius: 10)
+        } placeholder: {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(.white)
+                .aspectRatio(1, contentMode: .fit)
+                .padding(.horizontal, 20)
+                .shadow(radius: 10)
+        }
+        .padding(.top, 20)
+    }
+}
+
+private struct TitleAndDescriptionView: View {
+    let viewModel: ArchivePlaylistViewModel
+
+    var body: some View {
+        HStack {
+            Text(viewModel.playlistTitle)
+                .font(.system(size: 32, weight: .bold))
+            Spacer()
+        }
+        .padding(.horizontal, 24)
+        .padding(.top, 40)
+
+        Text(viewModel.playlistDescription)
+            .font(.system(size: 15))
+            .foregroundStyle(.gray)
+            .padding(10)
+            .background(.ultraThinMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .padding(.horizontal, 16)
+            .padding(.top, 16)
+    }
+}
+
+private struct PlaylistView: View {
+    @Bindable var viewModel: ArchivePlaylistViewModel
+
+    var body: some View {
+        HStack {
+            Text("플레이리스트")
+                .font(.system(size: 20, weight: .semibold))
+            Spacer()
+        }
+        .padding(.horizontal, 24)
+        .padding(.top, 28)
+
+        Divider()
+            .padding(.top, 10)
+
+        List {
+            ForEach(viewModel.dummyPlaylist) { song in
+                HStack {
+                    AsyncImage(url: song.imageURL) { image in
+                        image
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                            .clipShape(RoundedRectangle(cornerRadius: 16))
+
+                    } placeholder: {
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(.black)
+                            .frame(width: 60, height: 60)
+                    }
+                    .padding(.trailing, 10)
+
+                    VStack(alignment: .leading) {
+                        Text(song.title)
+                            .font(.system(size: 20))
+                        Text(song.artist)
+                            .font(.system(size: 16))
+                            .foregroundStyle(.gray)
+                    }
+                }
+            }
+            .onDelete(perform: viewModel.deleteMusic)
+            .onMove(perform: viewModel.moveMusic)
+        }
+        .listStyle(.plain)
+        .scrollDisabled(true)
+        .scaledToFit()
+    }
+}
+
+#Preview {
+    ArchivePlaylistView()
+}

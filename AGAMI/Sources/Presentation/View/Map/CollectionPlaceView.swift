ccd0991dc9614800f0f0112dba5822e3fb1cd777
ye-gang <yegang13209@naver.com>
2024-10-15 19:53:14 +0900

    [#11] CollectionPlace 에서 ViewModel과 분리

--- AGAMI/Sources/Presentation/View/Map/CollectionPlaceView.swift
+++ AGAMI/Sources/Presentation/View/Map/CollectionPlaceView.swift
@@ -7,28 +7,14 @@
 
 import SwiftUI
 
-struct PlayList: Identifiable, Hashable {
-    var id = UUID()
-    var address: String
-    var date: String
-    var time: String
-}
-
 struct CollectionPlaceView: View {
-    @State private var playList: [PlayList] = [
-        PlayList(address: "포항시", date: "2024-03-02", time: "17:00"),
-        PlayList(address: "천안시", date: "2023-03-01", time: "13:00"),
-        PlayList(address: "서울시", date: "2020-05-05", time: "10:00")
-    ]
     @Environment(\.dismiss) var dismiss
-    @State private var viewModel = MapViewModel()
+    @State private var viewModel = CollectionPlaceViewModel()
     
     var body: some View {
         ScrollView {
             VStack(spacing: 24) {
-                CollectionPlaceListView(playList: $playList)
-                
-                CollectionPlaceListView(playList: $playList)
+                CollectionPlaceListView(playList: $viewModel.playList)                
             }
         }
         .navigationTitle("수집 장소")

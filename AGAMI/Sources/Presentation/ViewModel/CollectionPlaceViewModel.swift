25f668fd8f0f155746087dd6ba5a1d1eb43c74d0
ye-gang <yegang13209@naver.com>
2024-10-15 19:52:52 +0900

    [#11] CollectionPlaceViewModel 구현(기존에 있던 CollectionPlace View에서 분리)

---
+++ AGAMI/Sources/Presentation/ViewModel/CollectionPlaceViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  CollectionPlaceViewModel.swift
+//  AGAMI
+//
+//  Created by yegang on 10/15/24.
+//
+
+import Foundation
+
+struct PlayList: Identifiable, Hashable {
+    var id = UUID()
+    var address: String
+    var date: String
+    var time: String
+}
+
+@Observable
+final class CollectionPlaceViewModel {
+    private let collectionPlaceService = CollectionPlaceService()
+    
+    var playList: [PlayList] = [
+        PlayList(address: "포항시", date: "2024-03-02", time: "17:00"),
+        PlayList(address: "천안시", date: "2023-03-01", time: "13:00"),
+        PlayList(address: "서울시", date: "2020-05-05", time: "10:00")
+    ]
+}

28905c012b9c5c4e49dbd5e917f8baf804313363
Guryss <lala1470@konkuk.ac.kr>
2024-10-15 17:02:24 +0900

    [#10] Search, Archive탭에서 공용으로 쓰이는 PlaylistRow 컴포넌트 생성

---
+++ AGAMI/Sources/Presentation/View/Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  AGAMI
+//
+//  Created by Seoyeon Choi on 10/15/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}

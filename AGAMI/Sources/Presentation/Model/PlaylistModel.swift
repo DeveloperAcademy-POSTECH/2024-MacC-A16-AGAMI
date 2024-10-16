3e7e268e9222391308969b350f301b75e26cfbcf
Sean <dnxogns0101@naver.com>
2024-10-16 15:52:41 +0900

    [#2] 코드리뷰 반영

--- AGAMI/Sources/Presentation/Model/PlaylistModel.swift
+++ AGAMI/Sources/Presentation/Model/PlaylistModel.swift
@@ -19,7 +19,16 @@ struct PlaylistModel: Codable, Identifiable {
     var longitude: Double
     var generationTime: Date
     
-    init(playlistID: UUID = UUID(), playlistName: String = "", description: String = "", photoURL: String = "", latitude: Double = 36.0126, longitude: Double = 129.3235, songs: [SongModel] = [], generationTime: Date = Date()) {
+    init(
+        playlistID: UUID = UUID(),
+        playlistName: String = "",
+        description: String = "",
+        photoURL: String = "",
+        latitude: Double = 36.0126,
+        longitude: Double = 129.3235,
+        songs: [SongModel] = [],
+        generationTime: Date = Date()
+    ) {
         self.playlistID = playlistID
         self.playlistName = playlistName
         self.description = description

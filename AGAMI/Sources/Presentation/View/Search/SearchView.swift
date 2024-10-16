54ba3c8f7e0cef133daa7814682b9fb769f1f45c
Guryss <lala1470@konkuk.ac.kr>
2024-10-16 16:26:40 +0900

    [#10] 변경된 SearchView UI 대응

--- AGAMI/Sources/Presentation/View/Search/SearchView.swift
+++ AGAMI/Sources/Presentation/View/Search/SearchView.swift
@@ -11,93 +11,32 @@ struct SearchView: View {
     @State private var isFind: Bool = false
     
     var body: some View {
-        VStack(spacing: 0) {
-            Button(action: {
-                isFind.toggle()
-            }, label: {
-                RoundedRectangle(cornerRadius: 10)
-                    .foregroundStyle(.gray.opacity(0.3))
-                    .overlay {
-                        Text(isFind ? "서치 중" : "서치 시작")
-                    }
-                    .frame(width: 260, height: 100)
-            })
-        }
-    }
-    
-    struct SearchTopView: View {
-        @Binding var isFind: Bool
-        
-        var body: some View {
+        NavigationStack {
             VStack(spacing: 0) {
-                Circle()
-                    .frame(width: 226, height: 226)
-                    .foregroundStyle(.secondary)
-                    .overlay {
-                        Circle()
-                            .frame(width: 74, height: 74)
-                            .foregroundStyle(isFind ? Color.gray : Color.white )
-                            .overlay {
-                                if isFind {
-                                    Image(systemName: "arrow.trianglehead.counterclockwise")
-                                        .foregroundStyle(.white)
-                                }
-                            }
-                            .onTapGesture {
-                                isFind.toggle()
-                            }
-                    }
-                    .background {
-                        if isFind {
-                            AsyncImage(url: URL(string: "https://i.pinimg.com/564x/6a/4f/45/6a4f45fc425ea299727b0a16d07da75e.jpg")) { image in
-                                image
-                                    .resizable()
-                                    .frame(width: 226, height: 226)
-                                    .clipShape(Circle())
-                            }
-                            placeholder: {
-                                ProgressView()
-                            }
+                Button(action: {
+                    isFind.toggle()
+                }, label: {
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundStyle(.gray.opacity(0.3))
+                        .overlay {
+                            Text(isFind ? "서치 중" : "서치 시작")
                         }
-                    }
-                    .padding(.bottom, 20)
-                
-                Text(isFind ? "Under The Influence" : "Listening ...")
-                    .font(.system(size: 24, weight: .semibold))
-                    .foregroundStyle(.black)
-                
-                Text("Chris Brown")
-                    .font(.system(size: 20, weight: .medium))
-                    .foregroundStyle(isFind ? .gray.opacity(0.5) : .white)
-                    .padding(.top, 4)
+                        .frame(width: 260, height: 100)
+                })
             }
-        }
-    }
-    
-    struct SearchPlaylistView: View {
-        var dummyPlaylist: [DummySongModel] = Array(repeating: .init(), count: 10)
-        
-        var body: some View {
-            HStack(spacing: 0) {
-                Spacer()
-                
-                Text("전체 삭제")
-                    .font(.system(size: 14, weight: .regular))
-                    .foregroundStyle(.gray.opacity(0.8))
-                    .padding(.trailing, 16)
-                    .padding(.bottom, 8)
+            .sheet(isPresented: $isFind, content: {
+                SearchPlaylistModalView(navigationTitle: "플레이리스트")
+            })
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing, content: {
+                    NavigationLink(destination: {
+                        SearchWritingView()
+                    }, label: {
+                        Text("다음")
+                    })
+                })
             }
-            
-            Divider()
-            
-            List {
-                ForEach(dummyPlaylist) { song in
-                    PlaylistRow(song: song)
-                }
-            }
-            .listStyle(.plain)
-            .scrollIndicators(.hidden)
-            .scaledToFit()
+            .navigationTitle("Search")
         }
     }
 }

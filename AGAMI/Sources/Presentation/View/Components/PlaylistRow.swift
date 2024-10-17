718ee0c561ba0ea9e5ffc65392e67be71945c508
Guryss <lala1470@konkuk.ac.kr>
2024-10-17 22:50:36 +0900

    [#22] PlaylistRow 변경 UI 대응

--- AGAMI/Sources/Presentation/View/Components/PlaylistRow.swift
+++ AGAMI/Sources/Presentation/View/Components/PlaylistRow.swift
@@ -11,25 +11,34 @@ struct PlaylistRow: View {
     let song: SongModel
     
     var body: some View {
-        HStack {
+        HStack(spacing: 0) {
             AsyncImage(url: URL(string: song.albumCoverURL)) { image in
                 image
                     .resizable()
                     .frame(width: 60, height: 60)
-                    .clipShape(RoundedRectangle(cornerRadius: 16))
 
             } placeholder: {
                 ProgressView()
             }
-            .padding(.trailing, 10)
+            .padding(.trailing, 20)
 
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 0) {
                 Text(song.title)
-                    .font(.system(size: 20))
+                    .font(.system(size: 17, weight: .semibold))
+                    .kerning(-0.43)
+                
                 Text(song.artist)
-                    .font(.system(size: 16))
+                    .font(.system(size: 15, weight: .regular))
+                    .kerning(-0.23)
                     .foregroundStyle(.gray)
             }
+            
+            Spacer()
+            
+            Image(systemName: "line.3.horizontal")
+                .foregroundStyle(.gray)
+                .font(.system(size: 16, weight: .regular))
+                .padding(.trailing, 15)
         }
     }
 }

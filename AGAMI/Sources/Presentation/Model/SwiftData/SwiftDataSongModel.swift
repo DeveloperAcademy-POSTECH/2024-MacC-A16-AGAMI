aefc6b5b231de6c1707fbab7c2add76bd04c8f5d
00yhsp <00yhsp@naver.com>
2024-10-19 13:53:33 +0900

    [#27] PersistenceService 생성중,,

--- AGAMI/Sources/Presentation/Model/SwiftData/SwiftDataSongModel.swift
+++ AGAMI/Sources/Presentation/Model/SwiftData/SwiftDataSongModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+final class SwiftDataSongModel {
+    @Attribute(.unique) var songID: String
+    var title: String
+    var artist: [String]
+    var albumCoverURL: String
+
+    init(
+        songID: String = "",
+        title: String = "",
+        artist: [String] = [],
+        albumCoverURL: String = ""
+    ) {
+        self.songID = songID
+        self.title = title
+        self.artist = artist
+        self.albumCoverURL = albumCoverURL
+    }
+}
--- AGAMI/Sources/Service/Persistence/PersistenceService.swift
+++ AGAMI/Sources/Service/Persistence/PersistenceService.swift
@@ -1,14 +1,75 @@
 //
-//  PersistenceService.swift
+//  PersistenceServiceImpl.swift
 //  AGAMI
 //
 //  Created by 박현수 on 10/18/24.
 //
 
 import Foundation
+import SwiftData
 
-protocol PersistenceService {
-    func createPlaylist()
-    func fetchPlaylist()
-    func deletePlaylist()
+final class PersistenceService {
+    let modelContainer: ModelContainer
+    let modelContext: ModelContext
+
+    init() {
+        let schema = Schema([
+            SwiftDataPlaylistModel.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        do {
+            modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+            modelContext = ModelContext(modelContainer)
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }
+
+    func createPlaylist(
+        playlistName: String?,
+        playlistDescription: String?,
+        photoURL: String?,
+        latitude: Double?,
+        longitude: Double?
+    ) throws {
+        let item = SwiftDataPlaylistModel(
+            playlistName: playlistName,
+            playlistDescription: playlistDescription,
+            photoURL: photoURL,
+            latitude: latitude,
+            longitude: longitude
+        )
+        modelContext.insert(item)
+        try modelContext.save()
+    }
+
+    func fetchPlaylist() throws -> [SwiftDataPlaylistModel] {
+        return try modelContext.fetch(FetchDescriptor<SwiftDataPlaylistModel>())
+    }
+
+    func updatePlaylistName(for item: SwiftDataPlaylistModel, to newPlaylistName: String) throws {
+        item.playlistName = newPlaylistName
+        try modelContext.save()
+    }
+
+    func updatePlaylistDescription(for item: SwiftDataPlaylistModel, to newPlaylistDescription: String) throws {
+        item.playlistDescription = newPlaylistDescription
+        try modelContext.save()
+    }
+
+    func updatePhotoURL(for item: SwiftDataPlaylistModel, to newPhotoURL: String) throws {
+        item.photoURL = newPhotoURL
+        try modelContext.save()
+    }
+
+    func updateCoordinates(for item: SwiftDataPlaylistModel, latitude: Double, longitude: Double) throws {
+        item.latitude = latitude
+        item.longitude = longitude
+        try modelContext.save()
+    }
+
+    func deletePlaylist(item: SwiftDataPlaylistModel) throws {
+        modelContext.delete(item)
+        try modelContext.save()
+    }
 }

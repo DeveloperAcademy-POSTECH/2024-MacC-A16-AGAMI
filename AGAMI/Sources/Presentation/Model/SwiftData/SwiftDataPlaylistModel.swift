fd253ec619e9436773c7e191e0be34f67736b4ad
00yhsp <00yhsp@naver.com>
2024-10-19 13:53:33 +0900

    [#27] PersistenceService 생성중,,

--- AGAMI/Sources/Presentation/Model/SwiftData/SwiftDataPlaylistModel.swift
+++ AGAMI/Sources/Presentation/Model/SwiftData/SwiftDataPlaylistModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+final class SwiftDataPlaylistModel {
+    @Attribute(.unique) var playlistID: UUID = UUID()
+    var playlistName: String?
+    var playlistDescription: String?
+    var photoURL: String?
+    var latitude: Double?
+    var longitude: Double?
+    var generationTime: Date = Date()
+
+    @Relationship var songs: [SongModel] = []
+
+    init(
+        playlistName: String?,
+        playlistDescription: String?,
+        photoURL: String?,
+        latitude: Double?,
+        longitude: Double?
+    ) {
+        self.playlistName = playlistName
+        self.playlistDescription = playlistDescription
+        self.photoURL = photoURL
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}

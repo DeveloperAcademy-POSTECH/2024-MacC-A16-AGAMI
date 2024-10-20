65b6d0c9431a38a554f102a7a742ba6e0e9c30f5
Guryss <lala1470@konkuk.ac.kr>
2024-10-20 17:13:17 +0900

    [#25] SearchStartView 내 커스텀 모달시트 기능 구현 중 ..

--- AGAMI/Sources/Presentation/View/Search/SearchStartView.swift
+++ AGAMI/Sources/Presentation/View/Search/SearchStartView.swift
@@ -10,37 +10,33 @@ import SwiftUI
 struct SearchStartView: View {
     @Environment(SearchCoordinator.self) var coordinator
     @State var viewModel: SearchStartViewModel = SearchStartViewModel()
-    
-    @State private var detent: PresentationDetent = .height(60)
-    
+
     var body: some View {
         VStack(spacing: 0) {
             Button {
-               // coordinator.presentSheet(.diggingModalView)
                 viewModel.searchButtonTapped()
             } label: {
                 RoundedRectangle(cornerRadius: 10)
                     .foregroundStyle(.gray.opacity(0.3))
                     .overlay {
-                        Text(viewModel.shazaming ? "서치 중" : "서치 시작")
+                        Text(viewModel.shazamStatus.buttonDescription)
                     }
                     .frame(width: 260, height: 100)
             }
         }
         .task {
-            viewModel.isFound = false
+            viewModel.showSheet = true
         }
-        .sheet(isPresented: .constant(true), onDismiss: {
-            print("onDismiss")
-            viewModel.isFound = false
-        })
-        {
-            SearchPlaylistModalView(viewModel: viewModel, navigationTitle: "플레이크 리스트")
+        .sheet(isPresented: $viewModel.showSheet, onDismiss: {
+            viewModel.shazamStatus = .idle
+        }) {
+            SearchPlaylistModalView(viewModel: viewModel)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
                 .presentationDetents([.height(60), .medium, .large])
                 .presentationCornerRadius(20)
                 .presentationBackgroundInteraction(.enabled(upThrough: .large))
-                .bottomMaskForSheet(mask: true)
+                .interactiveDismissDisabled()
+                .bottomMaskForSheet()
         }
         .navigationTitle("Plaking")
     }

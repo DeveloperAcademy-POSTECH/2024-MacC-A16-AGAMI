8f1a283c4c322a3da6cd9554254012071f2d7583
ye-gang <yegang13209@naver.com>
2024-10-21 02:44:15 +0900

    [#21] 사진을 찍고나면 카메라가 회전하는 에러를 해결하기 위해 주석처리

--- AGAMI/Sources/Presentation/View/Camera/CameraPreView.swift
+++ AGAMI/Sources/Presentation/View/Camera/CameraPreView.swift
@@ -32,7 +32,7 @@ struct CameraPreview: UIViewRepresentable {
         view.videoPreviewLayer.videoGravity = .resizeAspectFill // 카메라 프리뷰 ratio 조절(fit, fill)
         view.videoPreviewLayer.cornerRadius = 10 // 프리뷰 모서리에 CornerRadius를 결정
         view.videoPreviewLayer.session = session // 카메라 세션 지정(필수)
-        view.videoPreviewLayer.connection?.videoRotationAngle = .zero // 비디오 기본 방향 지정
+//        view.videoPreviewLayer.connection?.videoRotationAngle = .zero // 비디오 기본 방향 지정
 
         return view
     }

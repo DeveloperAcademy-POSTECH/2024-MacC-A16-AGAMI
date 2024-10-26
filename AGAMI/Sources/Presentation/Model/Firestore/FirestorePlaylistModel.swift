7287aceea711bd19347ca11a8b1e9ac7b63b7f97
00yhsp <00yhsp@naver.com>
2024-10-27 01:17:01 +0900

    [#49] ArchivePlaylistView List 레이아웃 | FirestorePlaylistModel CodingKey / Encode / Decode 구현

--- AGAMI/Sources/Presentation/Model/Firestore/FirestorePlaylistModel.swift
+++ AGAMI/Sources/Presentation/Model/Firestore/FirestorePlaylistModel.swift
@@ -27,6 +27,11 @@ struct FirestorePlaylistModel: PlaylistModel, Codable, Identifiable {
 
     var id: String { playlistID }
 
+    enum CodingKeys: String, CodingKey {
+        case firestoreDocumentID, playlistID, playlistName, playlistDescription
+        case photoURL, latitude, longitude, streetAddress, generationTime, firestoreSongs
+    }
+
     init(
         playlistID: String = UUID().uuidString,
         playlistName: String = "",
@@ -50,14 +55,42 @@ struct FirestorePlaylistModel: PlaylistModel, Codable, Identifiable {
     }
 
     init(from playlistModel: PlaylistModel) {
-        self.playlistID = playlistModel.playlistID
-        self.playlistName = playlistModel.playlistName
-        self.playlistDescription = playlistModel.playlistDescription
-        self.photoURL = playlistModel.photoURL
-        self.latitude = playlistModel.latitude
-        self.longitude = playlistModel.longitude
-        self.streetAddress = playlistModel.streetAddress
-        self.generationTime = playlistModel.generationTime
-        self.firestoreSongs = playlistModel.songs.map { FirestoreSongModel(from: $0) }
+        self.init(
+            playlistID: playlistModel.playlistID,
+            playlistName: playlistModel.playlistName,
+            playlistDescription: playlistModel.playlistDescription,
+            photoURL: playlistModel.photoURL,
+            latitude: playlistModel.latitude,
+            longitude: playlistModel.longitude,
+            streetAddress: playlistModel.streetAddress,
+            firestoreSongs: playlistModel.songs.map { FirestoreSongModel(from: $0) },
+            generationTime: playlistModel.generationTime
+        )
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.playlistID = try container.decodeIfPresent(String.self, forKey: .playlistID) ?? UUID().uuidString
+        self.playlistName = try container.decodeIfPresent(String.self, forKey: .playlistName) ?? ""
+        self.playlistDescription = try container.decodeIfPresent(String.self, forKey: .playlistDescription) ?? ""
+        self.photoURL = try container.decodeIfPresent(String.self, forKey: .photoURL) ?? ""
+        self.latitude = try container.decodeIfPresent(Double.self, forKey: .latitude) ?? 36.0135
+        self.longitude = try container.decodeIfPresent(Double.self, forKey: .longitude) ?? 129.3262
+        self.streetAddress = try container.decodeIfPresent(String.self, forKey: .streetAddress) ?? ""
+        self.generationTime = try container.decodeIfPresent(Date.self, forKey: .generationTime) ?? Date()
+        self.firestoreSongs = try container.decodeIfPresent([FirestoreSongModel].self, forKey: .firestoreSongs) ?? []
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(playlistID, forKey: .playlistID)
+        try container.encode(playlistName, forKey: .playlistName)
+        try container.encode(playlistDescription, forKey: .playlistDescription)
+        try container.encode(photoURL, forKey: .photoURL)
+        try container.encode(latitude, forKey: .latitude)
+        try container.encode(longitude, forKey: .longitude)
+        try container.encode(streetAddress, forKey: .streetAddress)
+        try container.encode(generationTime, forKey: .generationTime)
+        try container.encode(firestoreSongs, forKey: .firestoreSongs)
     }
 }

9c60dae9d4a58930a47991223aeb77d80ce18e98
Guryss <lala1470@konkuk.ac.kr>
2024-10-28 01:07:58 +0900

    [#62] SearchStartView 기본 UI 변경 대응

--- AGAMI/Sources/Presentation/View/Search/SearchStartView.swift
+++ AGAMI/Sources/Presentation/View/Search/SearchStartView.swift
@@ -12,16 +12,44 @@ struct SearchStartView: View {
     @State var viewModel: SearchStartViewModel = SearchStartViewModel()
 
     var body: some View {
-        VStack(spacing: 0) {
-            Button {
-                viewModel.searchButtonTapped()
-            } label: {
-                RoundedRectangle(cornerRadius: 10)
-                    .foregroundStyle(.gray.opacity(0.3))
-                    .overlay {
-                        Text(viewModel.shazamStatus.buttonDescription)
-                    }
-                    .frame(width: 260, height: 100)
+        ZStack {
+            Color(.pPrimary).ignoresSafeArea(edges: .top)
+            
+            VStack(spacing: 0) {
+                HStack(spacing: 0) {
+                    Image(.plakeLogo)
+                        .padding(.top, 24)
+                        .padding(.leading, 24)
+                    
+                    Spacer()
+                }
+                .padding(.bottom, 68)
+                
+                Button {
+                    viewModel.searchButtonTapped()
+                } label: {
+                    Image(.diggingButtonBackground)
+                        .resizable()
+                        .frame(width: 393, height: 393)
+                        .overlay {
+                            Image(.diggingButton)
+                                .resizable()
+                                .frame(width: 85, height: 85)
+                        }
+                }
+                
+                VStack(spacing: 5) {
+                    Text("플레이크를 눌러 디깅하기")
+                        .font(.pretendard(weight: .semiBold600, size: 24))
+                        .foregroundStyle(.white)
+                    
+                    Text("지금 들리는 노래를 디깅해보세요.")
+                        .font(.pretendard(weight: .medium500, size: 20))
+                        .foregroundStyle(.white)
+                }
+                .padding(.top, -50)
+                
+                Spacer()
             }
         }
         .onAppear {
@@ -31,7 +59,6 @@ struct SearchStartView: View {
         .onDisappear {
             coordinator.dismissSheet()
         }
-        .navigationTitle("Plaking")
     }
 }
 

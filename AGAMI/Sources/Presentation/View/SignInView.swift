2c9b1e73ce829c3f7c64b7cd0ea4e615c2be2d28
Sean <dnxogns0101@naver.com>
2024-10-28 17:40:46 +0900

    [#54] SignInView 디자인 반영

--- AGAMI/Sources/Presentation/View/SignInView.swift
+++ AGAMI/Sources/Presentation/View/SignInView.swift
@@ -14,21 +14,37 @@ struct SignInView: View {
     @State private var viewModel: SignInViewModel = SignInViewModel()
     
     var body: some View {
-        VStack {
-            Spacer()
+        ZStack {
+            Image(.signInBackground)
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
             
-            SignInWithAppleButton(.continue) { request in
-                viewModel.signInRequest(request: request)
-            } onCompletion: { result in
-                switch result {
-                case .success(let authorization):
-                    viewModel.handleSuccessfulLogin(with: authorization)
-                case .failure(let error):
-                    viewModel.handleLoginError(with: error)
+            VStack(spacing: 0) {
+                Spacer()
+                
+                Image(.signInLogo)
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 24, trailing: 20))
+                
+                Text("순간을 놓치지 않는 즐거움,\n나만의 플라키브 생성하기")
+                    .font(.pretendard(weight: .semiBold600, size: 18))
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4)
+                    .foregroundStyle(Color(.pBlack))
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 234, trailing: 20))
+                
+                SignInWithAppleButton(.continue) { request in
+                    viewModel.signInRequest(request: request)
+                } onCompletion: { result in
+                    switch result {
+                    case .success(let authorization):
+                        viewModel.handleSuccessfulLogin(with: authorization)
+                    case .failure(let error):
+                        viewModel.handleLoginError(with: error)
+                    }
                 }
+                .frame(height: 56)
+                .padding(EdgeInsets(top: 0, leading: 16, bottom: 80, trailing: 16))
             }
-            .frame(height: 56)
-            .padding(EdgeInsets(top: 0, leading: 20, bottom: 80, trailing: 20))
         }
     }
 }

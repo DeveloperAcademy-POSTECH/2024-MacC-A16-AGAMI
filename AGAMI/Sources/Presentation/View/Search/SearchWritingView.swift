92dbeb492d3e0006051a106e95df07b12f773e6c
Guryss <lala1470@konkuk.ac.kr>
2024-10-28 22:44:03 +0900

    [#62] EdgeInsets 익스텐션 활용 및 SearchWritingView 뷰 분리

--- AGAMI/Sources/Presentation/View/Search/SearchWritingView.swift
+++ AGAMI/Sources/Presentation/View/Search/SearchWritingView.swift
@@ -10,39 +10,11 @@ import SwiftUI
 struct SearchWritingView: View {
     @Environment(SearchCoordinator.self) var coordinator
     @State var viewModel: SearchWritingViewModel = SearchWritingViewModel()
-    @FocusState var isFocused: Bool
     @State private var showActionSheet: Bool = false
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 0) {
-                AsyncImage(url: URL(string: viewModel.photoUrl)) { image in
-                    image
-                        .resizable()
-                        .clipShape(RoundedRectangle(cornerRadius: 12))
-                        .aspectRatio(1, contentMode: .fit)
-                        .padding(.horizontal, 58)
-                        .shadow(color: .black.opacity(0.25), radius: 8, x: 0, y: 4)
-                } placeholder: {
-                    Image(.basicCover)
-                        .resizable()
-                        .clipShape(RoundedRectangle(cornerRadius: 13))
-                        .padding(.horizontal, 58)
-                        .shadow(color: .black.opacity(0.25), radius: 8, x: 0, y: 4)
-                        .overlay {
-                            HStack(spacing: 0) {
-                                Image(systemName: "photo.fill")
-                                    .font(.system(size: 17, weight: .regular))
-                                    .foregroundStyle(Color(.pPrimary))
-                                
-                                Text("커버 설정하기")
-                                    .font(.pretendard(weight: .semiBold600, size: 20))
-                                    .foregroundStyle(Color(.pPrimary))
-                                    .padding(.leading, 5)
-                            }
-                        }
-                }
-                .padding(.top, 18)
+        List {
+            PlaylistCoverImageView(viewModel: viewModel)
                 .onTapGesture {
                     if viewModel.photoUrl == "" {
                         coordinator.push(view: .cameraView(viewModel: viewModel))
@@ -50,41 +22,36 @@ struct SearchWritingView: View {
                         showActionSheet.toggle()
                     }
                 }
-                
-                // 실시간 위치 넣기
-                TextField("효자동에서 만난 플레이크", text: $viewModel.userTitle)
-                    .font(.pretendard(weight: .semiBold600, size: 24))
-                    .foregroundStyle(.black)
-                    .focused($isFocused)
-                    .padding(EdgeInsets(top: 15, leading: 16, bottom: 15, trailing: 8))
-                    .background(Color(.pLightGray))
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color(.pPrimary), lineWidth: isFocused ? 1 : 0)
-                    }
-                    .padding(.top, 30)
-                    .padding(.horizontal, 8)
-                
-                HStack(spacing: 0) {
-                    Text("수집한 플레이크")
-                        .font(.pretendard(weight: .semiBold600, size: 20))
-                        .foregroundStyle(Color(.pBlack))
-                    Spacer()
-                    Text("\(viewModel.diggingList.count) 플레이크")
-                        .font(.pretendard(weight: .medium500, size: 16))
-                        .foregroundStyle(Color(.pPrimary))
-                }
+                .listRowSeparator(.hidden)
+                .listRowInsets(.zero)
+            
+            PlaylistTitleTextField(viewModel: viewModel)
+                .padding(.top, 12)
+                .padding(.horizontal, 8)
+                .listRowSeparator(.hidden)
+                .listRowInsets(.zero)
+            
+            PlaylistSongListHeader(viewModel: viewModel)
                 .padding(.top, 40)
                 .padding(.horizontal, 24)
-                
-                List {
-                    ForEach(viewModel.diggingList, id: \.songID) { song in
-                        PlaylistRow(song: song)
-                    }
-                }
-                .listStyle(PlainListStyle())
+                .listRowSeparator(.hidden)
+                .listRowInsets(.zero)
+            
+            PlaylistSongList(viewModel: viewModel)
+                .padding(.top, 10)
                 .padding(.horizontal, 8)
+                .listRowInsets(.zero)
+            
+            PlaylistDescriptionTextField(viewModel: viewModel)
+                .padding(.horizontal, 8)
+                .padding(.vertical, 13)
+                .listRowSeparator(.hidden)
+                .listRowInsets(.zero)
+                .padding(.bottom, 56)
+        }
+        .confirmationDialog("", isPresented: $showActionSheet) {
+            Button("다시 찍기") {
+                coordinator.push(view: .cameraView(viewModel: viewModel))
                 
                 RoundedRectangle(cornerRadius: 10)
                     .foregroundStyle(Color(.pLightGray))
@@ -151,15 +118,10 @@ struct SearchWritingView: View {
                 }
                 .disabled(!viewModel.isLoaded)
             }
-            .confirmationDialog("", isPresented: $showActionSheet) {
-                Button("다시 찍기") {
-                    coordinator.push(view: .cameraView(viewModel: viewModel))
-                }
-                Button("기본 이미지로 변경", role: .destructive) {
-                    viewModel.photoUrl = ""
-                }
-                Button("취소", role: .cancel) {}
+            Button("기본 이미지로 변경", role: .destructive) {
+                viewModel.photoUrl = ""
             }
+            Button("취소", role: .cancel) {}
             .navigationTitle("플리카빙")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
@@ -187,7 +149,25 @@ struct SearchWritingView: View {
         .onTapGesture {
             hideKeyboard()
         }
+        .listStyle(PlainListStyle())
+        .navigationTitle("플리카빙")
+        .navigationBarTitleDisplayMode(.inline)
         .scrollIndicators(.hidden)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    Task {
+                        await viewModel.savedPlaylist()
+                        viewModel.clearDiggingList()
+                        coordinator.popToRoot()
+                    }
+                } label: {
+                    Text("저장")
+                        .font(.pretendard(weight: .semiBold600, size: 17))
+                }
+            }
+        }
+        .toolbarRole(.editor)
         .toolbarVisibilityForVersion(.hidden, for: .tabBar)
     }
     
@@ -225,5 +205,106 @@ struct SearchWritingView: View {
             .onTapGesture {
                 coordinator.push(view: .cameraView(viewModel: viewModel))
             }
+    }
+}
+
+private struct PlaylistCoverImageView: View {
+    var viewModel: SearchWritingViewModel
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            Spacer()
+            AsyncImage(url: URL(string: viewModel.photoUrl)) { image in
+                image
+                    .resizable()
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    .aspectRatio(1, contentMode: .fit)
+                    .frame(width: 277, height: 277)
+                    .shadow(color: .black.opacity(0.25), radius: 8, x: 0, y: 4)
+            } placeholder: {
+                Image(.basicCover)
+                    .resizable()
+                    .clipShape(RoundedRectangle(cornerRadius: 13))
+                    .frame(width: 277, height: 277)
+                    .shadow(color: .black.opacity(0.25), radius: 8, x: 0, y: 4)
+                    .overlay {
+                        HStack(spacing: 0) {
+                            Image(systemName: "photo.fill")
+                                .font(.system(size: 17, weight: .regular))
+                                .foregroundStyle(Color(.pPrimary))
+                            
+                            Text("커버 설정하기")
+                                .font(.pretendard(weight: .semiBold600, size: 20))
+                                .foregroundStyle(Color(.pPrimary))
+                                .padding(.leading, 5)
+                        }
+                    }
+            }
+            Spacer()
+        }
+        .padding(.vertical, 18)
+    }
+}
+
+private struct PlaylistTitleTextField: View {
+    @Bindable var viewModel: SearchWritingViewModel
+    @FocusState var isFocused: Bool
+    
+    var body: some View {
+        TextField("효자동에서 만난 플레이크", text: $viewModel.userTitle)
+            .font(.pretendard(weight: .semiBold600, size: 24))
+            .foregroundStyle(.black)
+            .focused($isFocused)
+            .padding(EdgeInsets(top: 15, leading: 16, bottom: 15, trailing: 8))
+            .background(Color(.pLightGray))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .overlay {
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color(.pPrimary), lineWidth: isFocused ? 1 : 0)
+            }
+    }
+}
+
+private struct PlaylistSongListHeader: View {
+    var viewModel: SearchWritingViewModel
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            Text("수집한 플레이크")
+                .font(.pretendard(weight: .semiBold600, size: 20))
+                .foregroundStyle(Color(.pBlack))
+            Spacer()
+            Text("\(viewModel.diggingList.count) 플레이크")
+                .font(.pretendard(weight: .medium500, size: 16))
+                .foregroundStyle(Color(.pPrimary))
+        }
+    }
+}
+
+private struct PlaylistSongList: View {
+    var viewModel: SearchWritingViewModel
+    
+    var body: some View {
+        ForEach(viewModel.diggingList, id: \.songID) { song in
+            PlaylistRow(song: song)
+        }
+        .listRowInsets(.init(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)))
+    }
+}
+
+private struct PlaylistDescriptionTextField: View {
+    @Bindable var viewModel: SearchWritingViewModel
+    @FocusState var isFocused: Bool
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .foregroundStyle(Color(.pLightGray))
+            .overlay(alignment: .topLeading) {
+                TextField("플레이크에 대한 설명 추가하기", text: $viewModel.userDescription, axis: .vertical)
+                    .background(.clear)
+                    .foregroundStyle(.black)
+                    .padding()
+            }
+            .frame(height: 65)
     }
 }

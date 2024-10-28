8f585f4ebe18184616cd274c8d2140463b31fb47
Guryss <lala1470@konkuk.ac.kr>
2024-10-28 22:44:03 +0900

    [#62] EdgeInsets 익스텐션 활용 및 SearchWritingView 뷰 분리

---
+++ AGAMI/Sources/Extensions/EdgeInsets+.swift
@@ -0,0 +1,14 @@
+//
+//  EdgeInsets+.swift
+//  AGAMI
+//
+//  Created by Seoyeon Choi on 10/28/24.
+//
+
+import SwiftUI
+
+extension EdgeInsets {
+    static var zero: EdgeInsets {
+        EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+    }
+}

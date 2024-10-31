17e88c5e629d0a2ba93056420559293bbbc371ac
ye-gang <yegang13209@naver.com>
2024-10-31 13:49:01 +0900

    [#74] MapView에 커스텀 어노케이션이 뜨게 반영

--- AGAMI/Sources/Presentation/View/Map/PlaceMarkerView.swift
+++ AGAMI/Sources/Presentation/View/Map/PlaceMarkerView.swift
@@ -8,37 +8,44 @@
 import SwiftUI
 
 struct PlaceMarkerView: View {
-    @State private var imagCnt = 3
+    let viewModel: MapViewModel
+    let playlist: PlaylistModel
     
     var body: some View {
-        BubbleView()
-            .overlay {
-                if imagCnt > 1 {
-                    ZStack {
-                        Circle()
-                            .frame(width: 30, height: 30)
-                        
-                        Text(String(imagCnt))
-                            .font(.title3)
-                            .foregroundStyle(.white)
-                    }
-                    .offset(x: 30, y: -35)
-                }
-            }
+        BubbleView(viewModel: viewModel, playlist: playlist)
+        // TODO: - 추후 클러스터링 도입후 바꿔야될지도 모르는 코드..
+//            .overlay {
+//                if imagCnt > 1 {
+//                    ZStack {
+//                        Circle()
+//                            .frame(width: 30, height: 30)
+//
+//                        Text(String(imagCnt))
+//                            .font(.title3)
+//                            .foregroundStyle(.white)
+//                    }
+//                    .offset(x: 30, y: -35)
+//                }
+//            }
     }
 }
 
 struct BubbleView: View {
+    let viewModel: MapViewModel
+    let playlist: PlaylistModel
+    
     var body: some View {
         VStack(spacing: 0) {
             ZStack {
                 RoundedRectangle(cornerRadius: 6)
                     .frame(width: 65, height: 65)
                 
-                Image("bear")
-                    .resizable()
-                    .frame(width: 55, height: 55)
-                    .clipShape(RoundedRectangle(cornerRadius: 4))
+                AsyncImage(url: URL(string: playlist.photoURL)) { image in
+                    image
+                        .image?.resizable()
+                        .frame(width: 55, height: 55)
+                        .clipShape(RoundedRectangle(cornerRadius: 4))
+                }
             }
             Triangle()
                 .frame(width: 10, height: 10)
@@ -66,6 +73,6 @@ struct Triangle: Shape {
     }
 }
 
-#Preview {
-    PlaceMarkerView()
-}
+//#Preview {
+//    PlaceMarkerView()
+//}

81cf4f6a9eba2d37ee0bf669cce2f283fdc8f98c
Guryss <lala1470@konkuk.ac.kr>
2024-11-01 21:27:38 +0900

    [#87] PlakeCTAButton 범용적으로 사용할 수 있도록 구현

---
+++ AGAMI/Sources/Presentation/View/Search/SearchShazamingView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchShazamingView.swift
+//  AGAMI
+//
+//  Created by Seoyeon Choi on 11/1/24.
+//
+
+import SwiftUI
+
+struct SearchShazamingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchShazamingView()
+}
---
+++ AGAMI/Sources/Presentation/ViewModel/Search/SearchShazamingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchShazamingViewModel.swift
+//  AGAMI
+//
+//  Created by Seoyeon Choi on 11/1/24.
+//
+
+import Foundation

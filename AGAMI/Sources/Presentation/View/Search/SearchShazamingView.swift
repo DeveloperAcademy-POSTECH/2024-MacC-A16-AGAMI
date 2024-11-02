dbe23ca41bf7a5525f96e68c985295728ab413e7
Guryss <lala1470@konkuk.ac.kr>
2024-11-02 15:43:15 +0900

    [#87] shazamBounce 로띠 파일 수정 및 SearchShazamingView UI 초기화

--- AGAMI/Sources/Presentation/View/Search/SearchShazamingView.swift
+++ AGAMI/Sources/Presentation/View/Search/SearchShazamingView.swift
@@ -14,39 +14,6 @@ struct SearchShazamingView: View {
         ZStack {
             Color(viewModel.shazamStatus.backgroundColor)
                 .ignoresSafeArea()
-                
-            VStack(spacing: 0) {
-                Spacer()
-                
-                ZStack {
-                    Image(.diggingButtonBackground)
-                    if viewModel.shazamStatus == .searching {
-                        CustomLottieView(.search)
-                            .ignoresSafeArea()
-                    }
-                    
-                    Button {
-                        viewModel.searchButtonTapped()
-                    } label: {
-                        Image(.shazamButton)
-                    }
-                }
-                if let title = viewModel.shazamStatus.title {
-                    Text(title)
-                        .font(.pretendard(weight: .semiBold600, size: 24))
-                        .foregroundStyle(Color(.pWhite))
-                }
-                
-                if let subTitle = viewModel.shazamStatus.subTitle {
-                    Text(subTitle)
-                        .font(.pretendard(weight: .medium500, size: 20))
-                        .foregroundStyle(Color(.pWhite))
-                }
-                
-                Spacer()
-                PlakeCTAButton(type: .cancel)
-                .padding(.bottom, 26)
-            }
         }
     }
 }

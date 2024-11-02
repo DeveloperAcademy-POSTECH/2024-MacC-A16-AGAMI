6842d6cffdbcfcc00afcd270ac6a5b6c9cd91e0b
Guryss <lala1470@konkuk.ac.kr>
2024-11-02 16:56:54 +0900

    [#87] SearchShazamingView 배경에 필요한 그라데이션 컬러 추가 및 세팅

---
+++ AGAMI/Sources/Helpers/GradientColors.swift
@@ -0,0 +1,14 @@
+//
+//  GradientColors.swift
+//  AGAMI
+//
+//  Created by Seoyeon Choi on 11/2/24.
+//
+
+import SwiftUI
+
+struct GradientColors {
+    static let pink = [Color(.pWhite), Color(.pinkGradient1), Color(.pinkGradient2), Color(.pinkGradient3), Color(.pinkGradient4)]
+    static let gray = [Color(.pWhite), Color(.grayGradient1), Color(.grayGradient2), Color(.grayGradient3), Color(.grayGradient4)]
+    static let failGray = [Color(.pWhite), Color(.failGrayGradient1), Color(.failGrayGradient2), Color(.failGrayGradient3), Color(.failGrayGradient4), Color(.failGrayGradient5), Color(.failGrayGradient6), Color(.pGray1)]
+}

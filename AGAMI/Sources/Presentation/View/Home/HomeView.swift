8d994d0910afab25a059a54c5756e5cce08296de
Guryss <lala1470@konkuk.ac.kr>
2024-11-04 16:23:02 +0900

    [#101] path에 뷰가 추가되면 커스텀 탭바가 hidden되도록 구현

--- AGAMI/Sources/Presentation/View/Home/HomeView.swift
+++ AGAMI/Sources/Presentation/View/Home/HomeView.swift
@@ -45,69 +45,71 @@ struct HomeView: View {
                     .environment(plakeCoordinator)
                 }
                 
-                HStack(spacing: 0) {
-                    Spacer()
-                    
-                    Image(systemName: "person.circle.fill")
-                        .font(.system(size: 39, weight: .regular))
-                        .foregroundStyle(Color(.pGray3))
-                        .onTapGesture {
-                            plakeCoordinator.push(route: .accountView)
-                        }
-                    
-                    Capsule()
-                        .foregroundStyle(Color(.pGray2))
-                        .frame(width: 179, height: 39)
-                        .overlay {
-                            HStack(spacing: 0) {
-                                Text("Plake")
-                                    .font(.pretendard(weight: .semiBold600, size: 13))
-                                    .kerning(-0.08)
-                                    .foregroundStyle(Color(.pGray1))
-                                    .onTapGesture {
-                                        withAnimation {
-                                            viewModel.selectedTab = .plake
-                                        }
-                                    }
-                                Spacer()
-                                
-                                Text("Map")
-                                    .font(.pretendard(weight: .semiBold600, size: 13))
-                                    .kerning(-0.08)
-                                    .foregroundStyle(Color(.pGray1))
-                                    .onTapGesture {
-                                        withAnimation {
-                                            viewModel.selectedTab = .map
-                                        }
-                                    }
+                if plakeCoordinator.path.isEmpty {
+                    HStack(spacing: 0) {
+                        Spacer()
+                        
+                        Image(systemName: "person.circle.fill")
+                            .font(.system(size: 39, weight: .regular))
+                            .foregroundStyle(Color(.pGray3))
+                            .onTapGesture {
+                                plakeCoordinator.push(route: .accountView)
                             }
-                            .padding(EdgeInsets(top: 0, leading: 29.25, bottom: 0, trailing: 32.25))
-                            .overlay(alignment: viewModel.selectedTab == .plake ? .leading : .trailing) {
-                                Capsule()
-                                    .foregroundStyle(Color(.pWhite))
-                                    .frame(width: 88, height: 35)
-                                    .shadow(color: Color(.pBlack).opacity(0.12), radius: 8, x: 0, y: 3)
-                                    .overlay {
-                                        Text(viewModel.selectedTab.title)
-                                            .font(.pretendard(weight: .semiBold600, size: 13))
-                                            .kerning(-0.08)
-                                            .foregroundStyle(Color(.pPrimary))
-                                    }
-                                    .padding(.horizontal, 2)
+                        
+                        Capsule()
+                            .foregroundStyle(Color(.pGray2))
+                            .frame(width: 179, height: 39)
+                            .overlay {
+                                HStack(spacing: 0) {
+                                    Text("Plake")
+                                        .font(.pretendard(weight: .semiBold600, size: 13))
+                                        .kerning(-0.08)
+                                        .foregroundStyle(Color(.pGray1))
+                                        .onTapGesture {
+                                            withAnimation {
+                                                viewModel.selectedTab = .plake
+                                            }
+                                        }
+                                    Spacer()
+                                    
+                                    Text("Map")
+                                        .font(.pretendard(weight: .semiBold600, size: 13))
+                                        .kerning(-0.08)
+                                        .foregroundStyle(Color(.pGray1))
+                                        .onTapGesture {
+                                            withAnimation {
+                                                viewModel.selectedTab = .map
+                                            }
+                                        }
+                                }
+                                .padding(EdgeInsets(top: 0, leading: 29.25, bottom: 0, trailing: 32.25))
+                                .overlay(alignment: viewModel.selectedTab == .plake ? .leading : .trailing) {
+                                    Capsule()
+                                        .foregroundStyle(Color(.pWhite))
+                                        .frame(width: 88, height: 35)
+                                        .shadow(color: Color(.pBlack).opacity(0.12), radius: 8, x: 0, y: 3)
+                                        .overlay {
+                                            Text(viewModel.selectedTab.title)
+                                                .font(.pretendard(weight: .semiBold600, size: 13))
+                                                .kerning(-0.08)
+                                                .foregroundStyle(Color(.pPrimary))
+                                        }
+                                        .padding(.horizontal, 2)
+                                }
                             }
-                        }
-                        .padding(.horizontal, 29)
-                    
-                    Image(systemName: "plus.circle.fill")
-                        .font(.system(size: 39, weight: .regular))
-                        .foregroundStyle(Color(.pPrimary))
-                        .onTapGesture {
-                            plakeCoordinator.push(route: .newPlakeView)
-                        }
-                    
-                    Spacer()
+                            .padding(.horizontal, 29)
+                        
+                        Image(systemName: "plus.circle.fill")
+                            .font(.system(size: 39, weight: .regular))
+                            .foregroundStyle(Color(.pPrimary))
+                            .onTapGesture {
+                                plakeCoordinator.push(route: .newPlakeView)
+                            }
+                        
+                        Spacer()
+                    }
+                    .padding(.bottom, 50)
                 }
-                .padding(.bottom, 50)
             }
         }
         .ignoresSafeArea(edges: .bottom)

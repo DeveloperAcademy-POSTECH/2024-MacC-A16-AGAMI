c5a51bb03d0ec60c8f3b16d4eb73387ddcc4e12f
Guryss <lala1470@konkuk.ac.kr>
2024-11-04 17:01:19 +0900

    [#106] 커스텀 탭바 UI 개선 및 Stack 구조 수정

--- AGAMI/Sources/Presentation/View/Home/HomeView.swift
+++ AGAMI/Sources/Presentation/View/Home/HomeView.swift
@@ -60,42 +60,38 @@ struct HomeView: View {
                     Capsule()
                         .foregroundStyle(Color(.pGray2))
                         .frame(width: 179, height: 39)
-                        .overlay {
-                            HStack(spacing: 0) {
-                                Text("Plake")
-                                    .font(.pretendard(weight: .semiBold600, size: 13))
-                                    .kerning(-0.08)
-                                    .foregroundStyle(Color(.pGray1))
-                                    .onTapGesture {
-                                        withAnimation {
-                                            viewModel.selectedTab = .plake
-                                        }
-                                    }
-                                Spacer()
-                                
-                                Text("Map")
-                                    .font(.pretendard(weight: .semiBold600, size: 13))
-                                    .kerning(-0.08)
-                                    .foregroundStyle(Color(.pGray1))
-                                    .onTapGesture {
-                                        withAnimation {
-                                            viewModel.selectedTab = .map
-                                        }
-                                    }
-                            }
-                            .padding(EdgeInsets(top: 0, leading: 29.25, bottom: 0, trailing: 32.25))
-                            .overlay(alignment: viewModel.selectedTab == .plake ? .leading : .trailing) {
+                        .overlay(alignment: .leading) {
+                            ZStack {
                                 Capsule()
                                     .foregroundStyle(Color(.pWhite))
                                     .frame(width: 88, height: 35)
                                     .shadow(color: Color(.pBlack).opacity(0.12), radius: 8, x: 0, y: 3)
-                                    .overlay {
-                                        Text(viewModel.selectedTab.title)
-                                            .font(.pretendard(weight: .semiBold600, size: 13))
-                                            .kerning(-0.08)
-                                            .foregroundStyle(Color(.pPrimary))
-                                    }
-                                    .padding(.horizontal, 2)
+                                    .offset(x: viewModel.selectedTab == .plake ? -43 : 43)
+                                    .animation(.easeInOut(duration: 0.3), value: viewModel.selectedTab)
+                                
+                                HStack(spacing: 0) {
+                                    Text("Plake")
+                                        .font(.pretendard(weight: .semiBold600, size: 13))
+                                        .kerning(-0.08)
+                                        .foregroundStyle(viewModel.selectedTab == .plake ? Color(.pPrimary) : Color(.pGray1))
+                                        .onTapGesture {
+                                            withAnimation {
+                                                viewModel.selectedTab = .plake
+                                            }
+                                        }
+                                    Spacer()
+                                    
+                                    Text("Map")
+                                        .font(.pretendard(weight: .semiBold600, size: 13))
+                                        .kerning(-0.08)
+                                        .foregroundStyle(viewModel.selectedTab == .map ? Color(.pPrimary) : Color(.pGray1))
+                                        .onTapGesture {
+                                            withAnimation {
+                                                viewModel.selectedTab = .map
+                                            }
+                                        }
+                                }
+                                .padding(EdgeInsets(top: 0, leading: 29.25, bottom: 0, trailing: 32.25))
                             }
                         }
                         .padding(.horizontal, 29)
@@ -111,6 +107,7 @@ struct HomeView: View {
                 }
                 .background(Color(.clear))
                 .padding(.bottom, 50)
+                
             }
         }
         .ignoresSafeArea(edges: .bottom)

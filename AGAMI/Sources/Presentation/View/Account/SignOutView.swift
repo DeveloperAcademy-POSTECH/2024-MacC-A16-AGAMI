75ada310d63ebbd094b12c522ee2ed8d9f6a54aa
ye-gang <yegang13209@naver.com>
2024-11-04 17:03:35 +0900

    [#99] 회원탈퇴 뷰 구현

--- AGAMI/Sources/Presentation/View/Account/SignOutView.swift
+++ AGAMI/Sources/Presentation/View/Account/SignOutView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct SignOutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(.pLightGray)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .center, spacing: 25) {
+                ZStack(alignment: .center) {
+                    Circle()
+                        .fill(Color(.pPrimary))
+                        .frame(width: 90, height: 90)
+                    
+                    Circle()
+                        .fill(Color(.pWhite))
+                        .frame(width: 82, height: 82)
+                    
+                    Image(systemName: "checkmark.circle.fill")
+                        .resizable()
+                        .frame(width: 76, height: 76)
+                        .foregroundStyle(Color(.pPrimary))
+                }
+                
+                Text("회원 탈퇴 완료!")
+                    .font(.pretendard(weight: .semiBold600, size: 24))
+                    .foregroundStyle(Color(.pBlack))
+            }
+        }
     }
 }
 

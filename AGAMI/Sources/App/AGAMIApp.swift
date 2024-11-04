371408fd146c18f75a51fc5da30d412efabe7c37
Guryss <lala1470@konkuk.ac.kr>
2024-11-04 22:16:42 +0900

    [#106] NavigationStack 하나만 있도록 수정 | PlakeCoordinator 수정

--- AGAMI/Sources/App/AGAMIApp.swift
+++ AGAMI/Sources/App/AGAMIApp.swift
@@ -4,6 +4,7 @@ import Firebase
 @main
 struct AGAMIApp: App {
     @AppStorage("isSignedIn") private var isSignedIn: Bool = false
+    @State private var plakeCoordinator: PlakeCoordinator = .init()
     
     init() {
         FirebaseApp.configure()
@@ -12,17 +13,34 @@ struct AGAMIApp: App {
     var body: some Scene {
         WindowGroup {
             if isSignedIn {
-                HomeView()
-                    .onOpenURL { url in
-                        if let redirectURL = Bundle.main.object(forInfoDictionaryKey: "REDIRECT_URL") as? String,
-                           let decodedRedirectURL = redirectURL.removingPercentEncoding {
-                            if url.absoluteString.contains(decodedRedirectURL) {
-                                SpotifyService.shared.handleURL(url)
-                            }
+                NavigationStack(path: $plakeCoordinator.path) {
+                    HomeView()
+                        .onOpenURL { url in
+                            handleURL(url)
                         }
-                    }
+                        .navigationDestination(for: PlakeRoute.self) { view in
+                            plakeCoordinator.build(route: view)
+                        }
+                        .sheet(item: $plakeCoordinator.sheet) { sheet in
+                            plakeCoordinator.buildSheet(sheet: sheet)
+                        }
+                        .fullScreenCover(item: $plakeCoordinator.fullScreenCover) { cover in
+                            plakeCoordinator.buildFullScreenCover(cover: cover)
+                        }
+                }
+                .environment(plakeCoordinator)
             } else {
                 SignInView()
+            }
+        }
+        
+    }
+    
+    private func handleURL(_ url: URL) {
+        if let redirectURL = Bundle.main.object(forInfoDictionaryKey: "REDIRECT_URL") as? String,
+           let decodedRedirectURL = redirectURL.removingPercentEncoding {
+            if url.absoluteString.contains(decodedRedirectURL) {
+                SpotifyService.shared.handleURL(url)
             }
         }
     }

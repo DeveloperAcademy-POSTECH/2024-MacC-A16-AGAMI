373ca41e792b9bbecf3ee6f30bb89144abd9147d
ye-gang <yegang13209@naver.com>
2024-11-05 16:32:40 +0900

    [#99] AccountViewModel 수정

--- AGAMI/Sources/Presentation/ViewModel/Account/AccountViewModel.swift
+++ AGAMI/Sources/Presentation/ViewModel/Account/AccountViewModel.swift
@@ -11,24 +11,29 @@ import PhotosUI
 @Observable
 final class AccountViewModel {
     private let firebaseAuthService: FirebaseAuthService = FirebaseAuthService()
+    private let firebaseService: FirebaseService = FirebaseService()
+    
     var isScucessDeleteAccount: Bool = false
+    
     var isEditMode: Bool = false
     var isPresented: Bool = false
     
     var isShowingSignOutAlert: Bool = false
     var isDeletingAccount: Bool = false
     
-    var userName: String = "가자미"
+    var userName: String?
+    var isUserNameSaved: Bool = false
     
     var showPhotoPicker: Bool = false
     var selectedItem: PhotosPickerItem?
-    var postImage: Image?
+    var postImage: UIImage?
+    var imageURL: String?
     
     func convertImage(item: PhotosPickerItem?) async {
         guard let item = item else { return }
         guard let data = try? await item.loadTransferable(type: Data.self) else { return }
         guard let uiImage = UIImage(data: data) else { return }
-        self.postImage = Image(uiImage: uiImage)
+        self.postImage = uiImage
     }
     
     func signOut() {
@@ -55,14 +60,55 @@ final class AccountViewModel {
         }
     }
     
-    func koreaLangCheck(_ input: String) -> Bool {
-        let pattern = "^[가-힣a-zA-Z\\s]*$"
-        if let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive) {
-            let range = NSRange(location: 0, length: input.utf16.count)
-            if regex.firstMatch(in: input, options: [], range: range) != nil {
-                return true
-            }
+    func saveUserName(nickname: String) async {
+        guard let uid = FirebaseAuthService.currentUID else {
+            dump("UID를 가져오는 데 실패했습니다.")
+            return
         }
-        return false
+        do {
+            try await firebaseService.saveUserNickname(userID: uid, nickname: nickname)
+            dump("닉네임을 저장하는데 성공했습니다.")
+        } catch {
+            dump("유저 닉네임을 저장하는데 실패했습니다.")
+        }
+    }
+    
+    func fetchUserInformation() async {
+        guard let uid = FirebaseAuthService.currentUID else {
+            dump("UID를 가져오는 데 실패했습니다.")
+            return
+        }
+        
+        do {
+            let userInformation = try await firebaseService.fetchUserInformation(userID: uid)
+            
+            if let userName = userInformation["UserNickname"] as? String {
+                self.userName = userName
+            } else {
+                dump("userName 값이 String 타입이 아니거나 nil입니다.")
+            }
+            
+            if let imageURL = userInformation["UserImageURL"] as? String {
+                self.imageURL = imageURL
+            } else {
+                dump("imageURL 값이 String 타입이 아니거나 nil입니다.")
+            }
+        } catch {
+            dump("유저 정보를 불러오는데 실패했습니다.")
+        }
+    }
+    
+    func saveUserProfileImage(image: UIImage) async {
+        guard let uid = FirebaseAuthService.currentUID else {
+            dump("UID를 가져오는 데 실패했습니다.")
+            return
+        }
+        
+        do {
+            try await firebaseService.uploadUserImageToFirebase(userID: uid, image: image)
+            dump("유저 이미지를 저장하는데 성공했습니다.")
+        } catch {
+            dump("유저 이미지를 저장하는데 실패했습니다.")
+        }
     }
 }

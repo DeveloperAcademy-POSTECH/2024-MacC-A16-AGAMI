1ac0da1be6a0720a19d76ba8de8fcd399568e9b8
Guryss <lala1470@konkuk.ac.kr>
2024-11-07 16:37:11 +0900

    [#122] PlakeList, PlakePlaylist 내 디자인 패딩 조정

--- AGAMI/Sources/Presentation/ViewModel/Plake/PlakePlaylistViewModel.swift
+++ AGAMI/Sources/Presentation/ViewModel/Plake/PlakePlaylistViewModel.swift
@@ -138,7 +138,7 @@ final class PlakePlaylistViewModel: Hashable {
 
     func formatDateToString(_ date: Date) -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy. MM. dd."
+        dateFormatter.dateFormat = "yyyy. MM. dd"
         return dateFormatter.string(from: date)
     }
 

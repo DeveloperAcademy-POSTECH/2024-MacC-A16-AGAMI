88f7b5fe5b08408272d7379af8a53a25911005f1
00yhsp <00yhsp@naver.com>
2024-11-17 23:15:34 +0900

    [#161] 인스타그램 스토리 공유 UI 및 로직 구현

--- AGAMI/Sources/Extensions/UIImage+.swift
+++ AGAMI/Sources/Extensions/UIImage+.swift
@@ -21,4 +21,38 @@ extension UIImage {
             self.draw(at: CGPoint(x: -squareRect.origin.x, y: -squareRect.origin.y))
         }
     }
+
+    func resizedAndCropped(to targetSize: CGSize) -> UIImage? {
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+
+        let originalSize = self.size
+        let widthRatio = targetSize.width / originalSize.width
+        let heightRatio = targetSize.height / originalSize.height
+
+        let scale = max(widthRatio, heightRatio)
+        let resizedSize = CGSize(width: originalSize.width * scale, height: originalSize.height * scale)
+
+        let xOffset = (resizedSize.width - targetSize.width) / 2
+        let yOffset = (resizedSize.height - targetSize.height) / 2
+        let cropRect = CGRect(x: -xOffset, y: -yOffset, width: resizedSize.width, height: resizedSize.height)
+
+        return renderer.image { _ in
+            self.draw(in: cropRect)
+        }
+    }
+
+    func applyBlur(radius: CGFloat = 20.0) -> UIImage? {
+        guard let ciImage = CIImage(image: self) else { return nil }
+
+        let filter = CIFilter(name: "CIGaussianBlur")
+        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+        filter?.setValue(radius, forKey: kCIInputRadiusKey)
+
+        guard let outputImage = filter?.outputImage else { return nil }
+
+        let context = CIContext()
+        guard let cgImage = context.createCGImage(outputImage, from: ciImage.extent) else { return nil }
+
+        return UIImage(cgImage: cgImage)
+    }
 }
---
+++ AGAMI/Sources/Presentation/View/Components/InstagramStickerView.swift
@@ -0,0 +1,165 @@
+//
+//  InstagramStickerView.swift
+//  AGAMI
+//
+//  Created by 박현수 on 11/17/24.
+//
+
+import SwiftUI
+
+struct InstagramStickerView: View {
+    let playlist: PlaylistModel
+    let images: [UIImage]
+
+    var body: some View {
+        switch images.count {
+        case ...0:
+            EmptyView()
+        case 1:
+            WithOneSong(playlist: playlist, images: images)
+        case 2:
+            WithTwoSongs(playlist: playlist, images: images)
+        default:
+            WithMultipleSongs(playlist: playlist, images: images)
+        }
+    }
+}
+
+private struct StickerImage: View {
+    let uiImage: UIImage?
+    var body: some View {
+        ZStack {
+            Image(.sharePhotoHolder)
+                .resizable()
+                .aspectRatio(1, contentMode: .fit)
+                .shadow(radius: 10)
+            if let image = uiImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+                    .padding(.horizontal, 36)
+            } else {
+                Image(.coverImageThumbnail)
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+                    .padding(.horizontal, 36)
+            }
+        }
+    }
+}
+
+private struct WithOneSong: View {
+    let playlist: PlaylistModel
+    let images: [UIImage]
+
+    var body: some View {
+        let title = playlist.songs.last?.title
+        let address = playlist.streetAddress.split(separator: ",").first?.trimmingCharacters(in: .whitespacesAndNewlines)
+
+        VStack(alignment: .leading, spacing: 0) {
+            StickerImage(uiImage: images.first)
+                .padding(.top, 70)
+
+            Spacer()
+
+            Text("\(title ?? "")")
+                .font(.pretendard(weight: .medium500, size: 24))
+                .foregroundStyle(Color(.pGray1))
+                .padding(.bottom, 6)
+
+            Text("\(address ?? "")에서\n수집한 플레이크입니다.")
+                .font(.pretendard(weight: .bold700, size: 32))
+                .foregroundStyle(Color(.pWhite))
+                .multilineTextAlignment(.leading)
+                .lineSpacing(4)
+                .padding(.bottom, 32)
+        }
+        .instagramStickerStyle()
+    }
+}
+
+private struct WithTwoSongs: View {
+    let playlist: PlaylistModel
+    let images: [UIImage]
+
+    var body: some View {
+        let title = playlist.songs.last?.title
+        let address = playlist.streetAddress.split(separator: ",").first?.trimmingCharacters(in: .whitespacesAndNewlines)
+
+        VStack(alignment: .leading, spacing: 0) {
+            ZStack {
+                Group {
+                    StickerImage(uiImage: images.first)
+                        .rotationEffect(.degrees(-5.98))
+                        .offset(x: -64)
+                    StickerImage(uiImage: images.last)
+                        .rotationEffect(.degrees(8.03))
+                        .offset(x: 64, y: 96)
+                }
+                .padding(.horizontal, 32)
+
+            }
+            .aspectRatio(1, contentMode: .fit)
+            .padding(.top, 70)
+
+            Spacer()
+
+            Text("\(title ?? "") 외 1곡")
+                .font(.pretendard(weight: .medium500, size: 24))
+                .foregroundStyle(Color(.pGray1))
+                .padding(.bottom, 6)
+
+            Text("\(address ?? "")에서\n수집한 플레이크입니다.")
+                .font(.pretendard(weight: .bold700, size: 32))
+                .foregroundStyle(Color(.pWhite))
+                .multilineTextAlignment(.leading)
+                .lineSpacing(4)
+                .padding(.bottom, 32)
+        }
+        .instagramStickerStyle()
+    }
+}
+
+private struct WithMultipleSongs: View {
+    let playlist: PlaylistModel
+    let images: [UIImage]
+
+    var body: some View {
+        let title = playlist.songs.last?.title
+        let address = playlist.streetAddress.split(separator: ",").first?.trimmingCharacters(in: .whitespacesAndNewlines)
+
+        VStack(alignment: .leading, spacing: 0) {
+            ZStack {
+                Group {
+                    StickerImage(uiImage: images.first)
+                        .rotationEffect(.degrees(0.86))
+                        .offset(x: 20, y: -40)
+                    StickerImage(uiImage: images.dropLast(1).last)
+                        .rotationEffect(.degrees(-12.4))
+                        .offset(x: -48, y: 64)
+                    StickerImage(uiImage: images.last)
+                        .rotationEffect(.degrees(3.28))
+                        .offset(x: 48, y: 96)
+                }
+                .padding(.horizontal, 32)
+            }
+            .aspectRatio(1, contentMode: .fit)
+            .padding(.top, 70)
+
+            Spacer()
+
+            Text("\(title ?? "") 외 2곡")
+                .font(.pretendard(weight: .medium500, size: 24))
+                .foregroundStyle(Color(.pGray1))
+                .padding(.bottom, 6)
+
+            Text("\(address ?? "")에서\n수집한 플레이크입니다.")
+                .font(.pretendard(weight: .bold700, size: 32))
+                .foregroundStyle(Color(.pWhite))
+                .multilineTextAlignment(.leading)
+                .lineSpacing(4)
+                .padding(.bottom, 32)
+        }
+        .instagramStickerStyle()
+    }
+}

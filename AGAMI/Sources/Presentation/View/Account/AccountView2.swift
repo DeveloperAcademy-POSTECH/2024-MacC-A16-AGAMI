07d59e774e26a2313ae61ecbec498b963bf02db4
ye-gang <yegang13209@naver.com>
2024-11-20 15:24:07 +0900

    [#169] 마이페이지 수정

---
+++ AGAMI/Sources/Presentation/View/Account/AccountView2.swift
@@ -0,0 +1,166 @@
+//
+//  AccountView2.swift
+//  AGAMI
+//
+//  Created by yegang on 11/19/24.
+//
+
+import SwiftUI
+
+struct AccountView2: View {
+    @State private var isSheetPresented = false
+    @State var isShowingSignOutAlert: Bool = false
+    @State var isShowingDeletingAccount: Bool = false
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HeaderView()
+            ContentView(isShowingDeletingAccount: $isShowingDeletingAccount, isShowingSignOutAlert: $isShowingSignOutAlert)
+        }
+        .ignoresSafeArea(edges: .bottom)
+        .alert("로그아웃", isPresented: $isShowingSignOutAlert) {
+            SignOutAlertActions()
+        } message: {
+            Text("로그아웃을 진행하시겠어요?\n다시 돌아오실 때를 기다릴게요!")
+        }
+        .alert("회원 탈퇴", isPresented: $isShowingDeletingAccount) {
+            DeleteAccountAlertActions()
+        } message: {
+            Text("회원 탈퇴 시 모든 기록이 삭제되고\n복구할 수 없습니다.")
+        }
+        
+    }
+}
+
+private struct HeaderView: View {
+    var body: some View {
+        ZStack(alignment: .center) {
+            Text("계정 관리")
+                .font(.pretendard(weight: .semiBold600, size: 20))
+            
+            HStack(spacing: 0) {
+                Button {
+                    // 닫기, 코디네이터 연결
+                } label: {
+                    Text("닫기")
+                        .font(.pretendard(weight: .regular400, size: 17))
+                        .foregroundStyle(.gray)
+                }
+                
+                Spacer()
+            }
+            .padding(.leading, 16)
+        }
+        .padding(.vertical, 20)
+    }
+}
+
+private struct ContentView: View {
+    @Environment(\.openURL) private var openURL
+    @Binding var isShowingDeletingAccount: Bool
+    @Binding var isShowingSignOutAlert: Bool
+    
+    var body: some View {
+        ZStack {
+            Color.green
+                .ignoresSafeArea()
+            
+            // 최상단 VStack (모든 버튼을 감싸는 스택)
+            VStack(spacing: 32) {
+                // 이용 약관, 회원 탈퇴 버튼이 묶여있는 스택
+                VStack(spacing: 14) {
+                    Button {
+                        // 이용 약관 알럿
+                        if let url = URL(string: "https://posacademy.notion.site/Plake-1302b843d5af81969d94daddfac63fde?pvs=4") {
+                            openURL(url)
+                        } else {
+                            dump("잘못된 URL입니다.")
+                        }
+                    } label: {
+                        ButtonLabel(
+                            title: "이용 약관",
+                            fontType: .pretendard(weight: .medium500, size: 17),
+                            fontColor: Color(.pBlack),
+                            backgroundColor: .white)
+                    }
+                    
+                    Button {
+                        // 회원 탈퇴 알럿
+                        isShowingDeletingAccount.toggle()
+                    } label: {
+                        ButtonLabel(
+                            title: "회원 탈퇴",
+                            fontType: .pretendard(weight: .medium500, size: 17),
+                            fontColor: Color(.pBlack),
+                            backgroundColor: .white)
+                    }
+                }
+                
+                Button {
+                    isShowingSignOutAlert.toggle()
+                } label: {
+                    ButtonLabel(
+                        title: "로그아웃",
+                        fontType: .pretendard(weight: .semiBold600, size: 17),
+                        fontColor: Color(.pWhite),
+                        backgroundColor: .black)
+                }
+                
+                Spacer()
+            }
+            .padding(EdgeInsets(top: 24, leading: 16, bottom: 0, trailing: 16))
+        }
+    }
+}
+
+private struct ButtonLabel: View {
+    let title: String
+    let fontType: Font
+    let fontColor: Color
+    let backgroundColor: Color
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            Text(title)
+                .font(fontType)
+                .foregroundStyle(fontColor)
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 17)
+        .background(
+            RoundedRectangle(cornerRadius: 4)
+                .fill(backgroundColor)
+        )
+    }
+}
+
+private struct SignOutAlertActions: View {
+    
+    var body: some View {
+        Button("취소", role: .cancel) {
+            //
+        }
+        
+        Button("로그아웃", role: .destructive) {
+            //
+        }
+    }
+}
+
+private struct DeleteAccountAlertActions: View {
+    
+    var body: some View {
+        Button("취소", role: .cancel) {
+            //
+        }
+        
+        Button("탈퇴하기", role: .destructive) {
+            //
+        }
+    }
+}
+
+
+#Preview {
+    AccountView2()
+}

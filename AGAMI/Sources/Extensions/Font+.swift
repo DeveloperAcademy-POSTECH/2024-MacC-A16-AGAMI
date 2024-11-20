b05db67620fe6d79e8968917721a2136791de898
Guryss <lala1470@konkuk.ac.kr>
2024-11-20 15:00:37 +0900

    [Chore] #167 - 폰트 에셋 추가

--- AGAMI/Sources/Extensions/Font+.swift
+++ AGAMI/Sources/Extensions/Font+.swift
@@ -10,6 +10,14 @@ extension Font {
     static func pretendard(weight: PretendardWeight, size: CGFloat) -> Font {
         return Font.custom(weight.fontName, size: size)
     }
+    
+    static func sCoreDream(weight: SCoreDreamWeight, size: CGFloat) -> Font {
+        return Font.custom(weight.fontName, size: size)
+    }
+    
+    static func notoSansKR(weight: NotoSansKRWeight, size: CGFloat) -> Font {
+        return Font.custom(weight.fontName, size: size)
+    }
 }
 
 enum PretendardWeight: String {
@@ -22,6 +30,38 @@ enum PretendardWeight: String {
     case bold700 = "Pretendard-Bold"
     case extraBold800 = "Pretendard-ExtraBold"
     case black900 = "Pretendard-Black"
+    
+    var fontName: String {
+        return self.rawValue
+    }
+}
+
+enum SCoreDreamWeight: String {
+    case dream1 = "SCoreDream1"
+    case dream2 = "SCoreDream2"
+    case dream3 = "SCoreDream3"
+    case dream4 = "SCoreDream4"
+    case dream5 = "SCoreDream5"
+    case dream6 = "SCoreDream6"
+    case dream7 = "SCoreDream7"
+    case dream8 = "SCoreDream8"
+    case dream9 = "SCoreDream9"
+    
+    var fontName: String {
+        return self.rawValue
+    }
+}
+
+enum NotoSansKRWeight: String {
+    case thin100 = "NotoSansKR-Thin"
+    case extraLight200 = "NotoSansKR-ExtraLight"
+    case light300 = "NotoSansKR-Light"
+    case regular400 = "NotoSansKR-Regular"
+    case medium500 = "NotoSansKR-Medium"
+    case semiBold600 = "NotoSansKR-SemiBold"
+    case bold700 = "NotoSansKR-Bold"
+    case extraBold800 = "NotoSansKR-ExtraBold"
+    case black900 = "NotoSansKR-Black"
     
     var fontName: String {
         return self.rawValue

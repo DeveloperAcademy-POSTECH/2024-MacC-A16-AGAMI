61e67725c3d76215837051ce51eab1694e6c2b18
Guryss <lala1470@konkuk.ac.kr>
2024-11-27 22:49:31 +0900

    [#212] 프리텐다드 폰트 열거형 제거 | 사용하지 않는 파일 삭제

--- AGAMI/Sources/Extensions/Font+.swift
+++ AGAMI/Sources/Extensions/Font+.swift
@@ -7,32 +7,12 @@
 import SwiftUI
 
 extension Font {
-    static func pretendard(weight: PretendardWeight, size: CGFloat) -> Font {
-        return Font.custom(weight.fontName, size: size)
-    }
-    
     static func sCoreDream(weight: SCoreDreamWeight, size: CGFloat) -> Font {
         return Font.custom(weight.fontName, size: size)
     }
     
     static func notoSansKR(weight: NotoSansKRWeight, size: CGFloat) -> Font {
         return Font.custom(weight.fontName, size: size)
-    }
-}
-
-enum PretendardWeight: String {
-    case thin100 = "Pretendard-Thin"
-    case extraLight200 = "Pretendard-ExtraLight"
-    case light300 = "Pretendard-Light"
-    case regular400 = "Pretendard-Regular"
-    case medium500 = "Pretendard-Medium"
-    case semiBold600 = "Pretendard-SemiBold"
-    case bold700 = "Pretendard-Bold"
-    case extraBold800 = "Pretendard-ExtraBold"
-    case black900 = "Pretendard-Black"
-    
-    var fontName: String {
-        return self.rawValue
     }
 }
 

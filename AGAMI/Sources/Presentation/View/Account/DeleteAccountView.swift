4c7c6f71726eff9613ea602281004cfcd04df33c
ye-gang <yegang13209@naver.com>
2024-11-28 14:41:01 +0900

    [#222] 탈퇴중일 때 로띠 추가와 삭제되고 화면 2초간 뜨게 변경

--- AGAMI/Sources/Presentation/View/Account/DeleteAccountView.swift
+++ AGAMI/Sources/Presentation/View/Account/DeleteAccountView.swift
@@ -13,25 +13,26 @@ struct DeleteAccountView: View {
     
     var body: some View {
         ZStack {
-            Color(.sTitleText)
-                .ignoresSafeArea()
+            Color(viewModel.deleteAccountProcess == .finished ? .sTitleText : .sMain)
             
-            VStack(alignment: .center, spacing: 44) {
-                Image(.deleteViewIcon)
-                    .resizable()
-                    .frame(maxWidth: 72, maxHeight: 78)
-                
-                switch viewModel.deleteAccountProcess {
-                case .none:
-                    EmptyView()
-                case .inProgress:
-                    //TODO: - 로띠 넣고 spacing이랑 폰트, 색상 수정 해야 됨
-                    VStack(alignment: .center, spacing: 0) {
-                        Text("회원 탈퇴중...")
-                            .font(.notoSansKR(weight: .semiBold600, size: 24))
-                            .foregroundStyle(Color(.sMain))
-                    }
-                default:
+            switch viewModel.deleteAccountProcess {
+            case .none:
+                EmptyView()
+            case .inProgress:
+                VStack(alignment: .center, spacing: 44) {
+                    CustomLottieView(.deleting)
+                        .frame(height: 67)
+                    
+                    Text("회원 탈퇴중...")
+                        .font(.notoSansKR(weight: .semiBold600, size: 24))
+                        .foregroundStyle(Color(.sTitleText))
+                }
+            default:
+                VStack(alignment: .center, spacing: 44) {
+                    Image(.deleteViewIcon)
+                        .resizable()
+                        .frame(maxWidth: 72, maxHeight: 78)
+                    
                     VStack(alignment: .center, spacing: 14) {
                         Text("회원 탈퇴")
                             .font(.notoSansKR(weight: .semiBold600, size: 24))
@@ -44,7 +45,7 @@ struct DeleteAccountView: View {
                             .kerning(-0.43)
                     }
                     .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                             UserDefaults.standard.removeObject(forKey: "isSignedIn")
                             coordinator.dismissSheet()
                             coordinator.popToRoot()

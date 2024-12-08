6e53430ffe9804985f132b3960134cd6ef641738
00yhsp <00yhsp@naver.com>
2024-12-08 14:35:14 +0900

    [#256] Info.plist 암호화 문서 추가

--- Project.swift
+++ Project.swift
@@ -77,6 +77,7 @@ extension InfoPlist {
                 "UIColorName": "",
                 "UIImageName": ""
             ],
+            "ITSAppUsesNonExemptEncryption": false,
             "NSAppleMusicUsageDescription": "플레이리스트 추가를 위해 Apple Music 라이브러리에 접근합니다.",
             "NSMicrophoneUsageDescription": "Shazam 음악 인식을 위해 마이크에 접근합니다.",
             "NSPhotoLibraryUsageDescription": "플레이리스트 사진 추가 및 사진 저장을 위해 사진 라이브러리에 접근합니다.",

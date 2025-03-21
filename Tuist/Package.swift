21470c0318953a73af6ada3d31feadf7bc52e8a0
ye-gang <yegang13209@naver.com>
2025-03-21 18:46:15 +0900

    [#291] 마크다운 라이브러리 추가

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.2"),
         .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.5.0"),
         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.22.0"),
-        .package(url: "https://github.com/onevcat/Kingfisher", from: "8.1.0")
+        .package(url: "https://github.com/onevcat/Kingfisher", from: "8.1.0"),
+        .package(url: "https://github.com/gonzalezreal/swift-markdown-ui", from: "2.0.2")
     ],
     targets: [
         .target(
@@ -33,6 +34,7 @@ let package = Package(
                 .product(name: "SpotifyAPI", package: "SpotifyAPI"),
                 .product(name: "KeychainAccess", package: "KeychainAccess"),
                 .product(name: "Lottie", package: "lottie-ios"),
+                .product(name: "MarkdownUI", package: "swift-markdown-ui"),
             ],
             path: "AGAMI/Sources"
         )
